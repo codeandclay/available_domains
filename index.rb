96bc3eb125bc984b06e19308958418cd90ed9134
codeandclay <oliver@codeandclay.com>
2021-03-20 17:24:48 +0000

    Build page from template

---
+++ index.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'mustache'
+require 'net/http'
+
+class Index < Mustache
+  def timestamp
+    json['updated']
+  end
+
+  def domains
+    json['domains']
+  end
+
+  private
+
+  def uri
+    URI("https://raw.githubusercontent.com/codeandclay/goodi.sh-names/main/available_sh_domains.json")
+  end
+
+  def json
+    @json ||= JSON.parse(Net::HTTP.get_response(uri).body)
+  end
+end
+
+File.open('index.html','w') { |f| f.puts Index.render }
